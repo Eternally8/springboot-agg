a82ed35263d48f8adff70a69b2c1ad051cc68009
hujingzheng <jingzheng.hu@weimob.com>
2021-12-28 18:27:14 +0800

    关闭健康检查

--- middleware/shardingJDBC-table2-Use/src/main/java/com/robben/ShardingApplication2.java
+++ middleware/shardingJDBC-table2-Use/src/main/java/com/robben/ShardingApplication2.java
@@ -3,6 +3,10 @@ package com.robben;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 注意如果开启了健康检查,则有可能需要关闭db的健康检查不然可能报错
+ *  management.health.db.enabled=false
+ */
 
 @SpringBootApplication
 public class ShardingApplication2 {
