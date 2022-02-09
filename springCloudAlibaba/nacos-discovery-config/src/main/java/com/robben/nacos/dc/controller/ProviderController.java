5aeec9eca5d70f468d87cf53c1bb9010689582b9
hujingzheng <jingzheng.hu@weimob.com>
2022-02-09 18:12:37 +0800

    升级nacos和dubbo2

--- springCloudAlibaba/nacos-discovery-config/src/main/java/com/robben/nacos/dc/controller/ProviderController.java
+++ springCloudAlibaba/nacos-discovery-config/src/main/java/com/robben/nacos/dc/controller/ProviderController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/nprovider")
 public class ProviderController {
 
-    @Value("${testValue:123}")
+    @Value("${testValue}")
     private String testValue;
 
 
