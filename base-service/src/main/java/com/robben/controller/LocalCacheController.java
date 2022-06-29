487330045aba05d2feb799d5e237220d2838fe98
hjz <hjz@alibaba-inc.com>
2022-06-29 15:14:55 +0800

    增加本地缓存测试案例3

--- base-service/src/main/java/com/robben/controller/LocalCacheController.java
+++ base-service/src/main/java/com/robben/controller/LocalCacheController.java
@@ -10,30 +10,37 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Api(tags = "本地缓存")
 @RestController
-@RequestMapping("/localhost")
+@RequestMapping("/localCache")
 public class LocalCacheController extends UnifiedReply {
     @Autowired
-    private CacheManager cacheManager;
+    @Qualifier("caffeineCacheManager")
+    private CacheManager caffeineCacheManager;
+
     @Autowired
     private LocalCacheService localCacheService;
 
 
-    @ApiOperation(value = "本地缓存使用")
-    @GetMapping(value = "/use")
-    public String use(){
+    @ApiOperation(value = "guava本地缓存使用")
+    @GetMapping(value = "/guavaCacheUse")
+    public String guavaCacheUse(){
         GuavaCacheUtils.setKey("aaa","bbb");
 
         String one = GuavaCacheUtils.getKey("aaa");
@@ -51,24 +58,50 @@ public class LocalCacheController extends UnifiedReply {
     }
 
 
-    @ApiOperation("本地缓存2")
-    @GetMapping(value = "/localCache")
-    public ResponseEntityDto<Boolean> localCache(@RequestParam String s){
+    @ApiOperation("caffeine本地缓存使用")
+    @GetMapping(value = "/caffeineCacheUse")
+    public ResponseEntityDto<Boolean> caffeineCacheUse(@RequestParam String s){
         return buildSuccesResp(localCacheService.getCacheValue(s,s));
     }
 
-    @ApiOperation("本地缓存3")
-    @GetMapping(value = "/localCache3")
-    public ResponseEntityDto<Boolean> localCache3(@RequestParam String s){
+
+    @ApiOperation("caffeine本地缓存使用2")
+    @GetMapping(value = "/caffeineCacheUse2")
+    public ResponseEntityDto<Boolean> caffeineCacheUse2(@RequestParam String s){
+        localCacheService.getCacheValue(s,s);
         localCacheService.getCacheValue(s,s);
 
-        Collection<String> cacheNames = cacheManager.getCacheNames();
+        //获取当前的缓存服务
+        Collection<String> cacheNames = caffeineCacheManager.getCacheNames();
         System.out.println(JSON.toJSONString(cacheNames));
 
-        Cache cacheManager1 = cacheManager.getCache("cacheManager");
-        System.out.println(cacheManager1.getName());
+        //可以在此打断点直接查看Cache中缓存的值
+        Cache cache = caffeineCacheManager.getCache("userCache");
+        Map<String, Object> stringObjectMap = cacheToMap(cache);
+        System.out.println(JSON.toJSONString(stringObjectMap));
 
         return buildSuccesResp();
     }
+
+
+    //通过方法获取所有缓存的key和value
+    public static Map<String, Object> cacheToMap(Cache cache) {
+        Object obj = cache.getNativeCache();
+        Map<String, Object> map = new HashMap<>();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                map.put(field.getName(), field.get(obj));
+            }
+        } catch (Exception e) {
+            log.error("ObjToMap exp", e);
+            return null;
+        }
+        // 获取Cache.map中的cache
+        return (Map<String, Object>) map.get("cache");
+    }
+
+
 
 }
